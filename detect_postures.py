7cb4167c0bde36d678c22b7949d94890a2382f9a
Ning Wang <30329277@qq.com>
2024-12-21 09:31:40 +0800

    change name of train_model_xxx , improve logic of detect_posture

--- detect_postures.py
+++ detect_postures.py
@@ -8,6 +8,7 @@ from tqdm import tqdm
 import json
 import subprocess
 import time
+import shlex
 
 def load_models(model_dir):
     """加载所有姿势检测模型."""
@@ -24,7 +25,7 @@ def load_models(model_dir):
             models[model_name] = model
     return models, device
 
-def detect_postures(frame, models, device):
+def detect_postures(frame, models, device, label, score_threshold):
     """检测图像帧中的所有姿势."""
     input_tensor = F.to_tensor(frame).unsqueeze(0).to(device)
     detected_postures = []
@@ -32,17 +33,24 @@ def detect_postures(frame, models, device):
         with torch.no_grad():
             predictions = model(input_tensor)
         for i, score in enumerate(predictions[0]['scores']):
-            if predictions[0]['labels'][i] == 1 and score > 0.5:
+            if predictions[0]['labels'][i] == label and score > score_threshold:
                 detected_postures.append(model_name)
                 break  # Once a posture is detected, move to the next model
     return detected_postures
 
-def process_video(video_path, models, device, output_folder, results):
+def format_time(seconds):
+    """格式化时间为 时:分:秒."""
+    hours = int(seconds // 3600)
+    minutes = int((seconds % 3600) // 60)
+    seconds = seconds % 60
+    return f"{hours:02}:{minutes:02}:{seconds:06.3f}"
+
+def process_video(video_path, models, device, output_folder, results, label, score_threshold, interval_seconds):
     """处理单个视频."""
     cap = cv2.VideoCapture(video_path)
     fps = cap.get(cv2.CAP_PROP_FPS)
     total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    interval_frames = int(30 * fps)  # 30 seconds interval
+    interval_frames = int(interval_seconds * fps)  # Interval in seconds
     frame_count = 0
     video_name = os.path.splitext(os.path.basename(video_path))[0]
     video_results = []
@@ -54,10 +62,11 @@ def process_video(video_path, models, device, output_folder, results):
                 break
 
             if frame_count % interval_frames == 0:
-                detected_postures = detect_postures(frame, models, device)
+                detected_postures = detect_postures(frame, models, device, label, score_threshold)
                 timestamp = frame_count / fps
+                formatted_timestamp = format_time(timestamp)
                 if detected_postures:
-                    video_results.append({"video": video_name, "time": timestamp, "postures": detected_postures})
+                    video_results.append({"video": video_name, "time": formatted_timestamp, "postures": detected_postures})
 
             frame_count += 1
             pbar.update(1)
@@ -65,21 +74,25 @@ def process_video(video_path, models, device, output_folder, results):
     cap.release()
     results[video_name] = video_results
 
+def time_to_seconds(time_str):
+    """将时间字符串转换为秒."""
+    hours, minutes, seconds = map(float, time_str.split(':'))
+    return hours * 3600 + minutes * 60 + seconds
 
-def extract_and_merge_segments(input_folder, output_folder, results):
+def extract_and_merge_segments(input_folder, output_folder, results, segment_gap_seconds):
     """提取并合并视频片段."""
-    # 使用FFmpeg的绝对路径,  请修改为你电脑上FFmpeg的实际路径
-    ffmpeg_path = r"D:\PythonProject\TorchTrainer\ffmpeg-master-latest-win64-gpl\bin\ffmpeg.exe"  #  替换为你的FFmpeg可执行文件的绝对路径
+    # 使用FFmpeg的绝对路径, 请修改为你电脑上FFmpeg的实际路径
+    ffmpeg_path = r"D:\PythonProject\TorchTrainer\ffmpeg-master-latest-win64-gpl\bin\ffmpeg.exe"  # 替换为你的FFmpeg可执行文件的绝对路径
 
     for video_name, video_data in results.items():
         segments = []
         start_time = -1
         for i, data in enumerate(video_data):
             if start_time == -1:
-                start_time = data['time']
+                start_time = time_to_seconds(data['time'])
 
-            if i + 1 == len(video_data) or video_data[i+1]['time'] - data['time'] > 180:
-                segments.append((start_time, data['time']))
+            if i + 1 == len(video_data) or time_to_seconds(video_data[i+1]['time']) - time_to_seconds(data['time']) > segment_gap_seconds:
+                segments.append((start_time, time_to_seconds(data['time'])))
                 start_time = -1
 
         if not segments:
@@ -90,7 +103,7 @@ def extract_and_merge_segments(input_folder, output_folder, results):
         temp_files = []
 
         for i, (start, end) in enumerate(segments):
-            #  处理开始和结束时间相同的情况
+            # 处理开始和结束时间相同的情况
             if start == end:
                 end += 0.1  # Add a small duration (0.1 seconds) to the end time
 
@@ -102,26 +115,36 @@ def extract_and_merge_segments(input_folder, output_folder, results):
             except subprocess.CalledProcessError as e:
                 print(f"Error executing FFmpeg: {e}")
                 # Handle the error appropriately, e.g., log it, skip the segment, etc.
-                continue  # Skip to the next segment            
+                continue  # Skip to the next segment
 
-        with open(os.path.join(output_folder, "concat_list.txt"), "w") as f:
+        concat_list_path = os.path.join(output_folder, "concat_list.txt")
+        with open(concat_list_path, "w", encoding="utf-8") as f:
             for temp_file in temp_files:
-                f.write(f"file '{os.path.abspath(temp_file)}'\n")  # 使用绝对路径
+                f.write(f"file '{os.path.abspath(temp_file)}'\n")  # 使用绝对路径并引用
 
         # 使用绝对路径
-        concat_list_path = os.path.abspath(os.path.join(output_folder, "concat_list.txt"))
-        cmd = [ffmpeg_path, "-f", "concat", "-safe", "0", "-i", concat_list_path, "-c", "copy", output_video]
-        subprocess.run(cmd, check=True)
+        concat_list_path = os.path.abspath(concat_list_path)
+        cmd = [ffmpeg_path, "-f", "concat", "-safe", "0", "-i", concat_list_path, "-fflags", "+genpts", "-c", "copy", output_video]
+        try:
+            subprocess.run(cmd, check=True)
+        except subprocess.CalledProcessError as e:
+            print(f"Error executing FFmpeg: {e}")
 
         for temp_file in temp_files:
             os.remove(temp_file)
-        os.remove(os.path.join(output_folder, "concat_list.txt"))
-
+        os.remove(concat_list_path)
 
 def main():
-    input_folder = "input_video"
-    output_folder = "output_video"
+    # input_folder = "input_video"
+    # output_folder = "output_video"
+    input_folder = r"\\192.168.31.1\XiaoMi-usb0\newdownload\2024-2\新建文件夹"
+    output_folder = r"\\192.168.31.1\XiaoMi-usb0\newdownload\2024-2\新建文件夹"
     result_file = os.path.join(input_folder, "result.json")
+    
+    label = 1
+    score_threshold = 0.7
+    interval_seconds = 60*1  # Interval in seconds for processing frames
+    segment_gap_seconds = 60*3  # Gap in seconds to consider segments separate
 
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
@@ -132,13 +155,12 @@ def main():
     for filename in os.listdir(input_folder):
         if filename.lower().endswith(('.mp4', '.avi', '.mov')): # Handle various video extensions
             video_path = os.path.join(input_folder, filename)
-            process_video(video_path, models, device, output_folder, results)
+            process_video(video_path, models, device, output_folder, results, label, score_threshold, interval_seconds)
 
     with open(result_file, "w") as f:
         json.dump(results, f, indent=4)
 
-    extract_and_merge_segments(input_folder, output_folder, results)
-
+    extract_and_merge_segments(input_folder, output_folder, results, segment_gap_seconds)
 
 if __name__ == "__main__":
     mp.freeze_support()
